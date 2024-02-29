7bc912d8c7f33940542983f49ba9c08d9cdfdf23
Jeiferson Almeida <jeiferson.almeida@americanas.io>
2024-02-29 11:53:03 -0300

    Subindo para teste remoto.

--- src/main/java/br/com/jeiferson/rinhabackendimperative/RinhabackendImperativeApplication.java
+++ src/main/java/br/com/jeiferson/rinhabackendimperative/RinhabackendImperativeApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RinhabackendImperativeApplication {
 
+	static {
+		System.out.println("Logging tuning properties...");
+		System.out.println("VIRTUAL_THREADS: " + System.getenv("VIRTUAL_THREADS"));
+		System.out.println("POOL_SIZE: " + System.getenv("POOL_SIZE"));
+		System.out.println("TOMCAT_THREAD_POOL: " + System.getenv("TOMCAT_THREAD_POOL"));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(RinhabackendImperativeApplication.class, args);
 	}
